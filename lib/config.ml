dbdd93397defdd869c92eda9e2aa97b576b2c948
Erhan Gundogan <erhan.gundogan@gmail.com>
2020-05-17 15:23:11 +0200

    updated readme (#1)

--- lib/config.ml
+++ lib/config.ml
@@ -10,7 +10,7 @@ let graphql_server = {
   port = 9876
 }
 let web_client_headers = [
-  ("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.13; rv:61.0) Gecko/20100101 Firefox/74.0");
+  ("user-agent", "Webcamel");
   ("accept", "text/html");
   ("accept-language", "en-US,en;q=0.5")
 ]
